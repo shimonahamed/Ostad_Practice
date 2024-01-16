9831b78235e0ff8b2cfa0fdfc048acc94d08427b
jibonahamed <smjibonahamed06@gmail.com>
2024-01-16 21:00:55 +0600

    frist commit

---
+++ ES6/decision_meking.js
@@ -0,0 +1,14 @@
+let parson={nam:'jibon',age:20,smile:true,shirtColor:'white'}
+
+if(parson['shirtColor']=="red"){
+    console.log("shirt Color is Red")
+}
+else if(parson['shirtColor']=="yellow"){
+    console.log("shirt Color is yellow")
+}
+else if(parson['shirtColor']=="white"){
+    console.log("shirt Color is white")
+}
+else{
+    console.log("shirt Color is not found")
+}
